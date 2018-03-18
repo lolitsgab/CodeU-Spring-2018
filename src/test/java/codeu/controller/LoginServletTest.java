0f8dff81f59248dfe28dcec84ca9df995273b650
Alison Tsoi <tsoialison@gmail.com>
2018-03-17 22:49:34 -0700

    attempting to fix error message in how returning string instead of bool in the mock equals functions

--- src/test/java/codeu/controller/LoginServletTest.java
+++ src/test/java/codeu/controller/LoginServletTest.java
@@ -107,7 +107,7 @@ public class LoginServletTest {
     // when user calls get password should return testpassword
     User mockUser = Mockito.mock(User.class);
     Mockito.when(mockUser.getPassword()).thenReturn("testpassword");
-    
+
     loginServlet.setUserStore(mockUserStore);
 
     HttpSession mockSession = Mockito.mock(HttpSession.class);
@@ -136,7 +136,7 @@ public void TestdoPost_InvalidPassword() throws IOException, ServletException {
   // test username's password should be testpassword not bad password
   User mockUser = Mockito.mock(User.class);
   String password = mockRequest.getParameter("password");
-  Mockito.doReturn(false).when(password.equals(mockUser.getPassword())))
+  Mockito.doReturn(false).when(password.equals(mockUser.getPassword()));
   loginServlet.setUserStore(mockUserStore);
 
   //HttpSession mockSession = Mockito.mock(HttpSession.class);
