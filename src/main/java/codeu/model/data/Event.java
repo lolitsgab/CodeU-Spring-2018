246967296c52164bbe6d6da156eda6c21446c18d
Celestine Co <celestine@Celestines-MacBook-Air.local>
2018-03-26 18:26:39 -0700

    New Event model class for activity feed

---
+++ src/main/java/codeu/model/data/Event.java
@@ -0,0 +1,45 @@
+package codeu.mode.data;
+
+/** Class representing an activity feed event. */
+public class Event {
+  private final UUID id;
+  private final UUID eventUser;
+  private final String eventType; /** Later make into subclass if we want */
+  private final Instant creation;
+
+  /**
+   * Constructs a new Event.
+   *
+   * @param id the ID of this Event
+   * @param eventUser the ID of the User associated with this Event
+   * @param eventType the type of this Event
+   * @param creation the creation time of this Event
+   */
+  public User(UUID id, UUID eventUser, String eventType, Instant creation) {
+    this.id = id;
+    this.eventUser = eventUser;
+    this.eventType = eventType;
+    this.creation = creation;
+  }
+
+  /** Returns the ID of this Event. */
+  public UUID getId() {
+    return id;
+  }
+
+  /** Returns the ID of the User associated with this Event. */
+  public UUID getEventUserId() {
+    return eventUser;
+  }
+
+  /** Returns the type of this Event. */
+  public String getEventType() {
+    return eventType;
+  }
+
+  /** Returns the creation time of this Event. */
+  public Instant getCreationTime() {
+    return creation;
+  }
+
+}
